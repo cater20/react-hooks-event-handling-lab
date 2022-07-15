11aad08bc8b58b4a34e56e9558a15632227af1db
chrisphine <onyangoketta@gmail.com>
2022-07-15 11:21:56 +0300

    input function

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -2,5 +2,14 @@
 import React from "react";
 
 function EyesOnMe(){
+return <button onFocus={focus} onBlur={blur}>Eyes On Me </button>
+
+function focus(){
+    console.log("Good!");
 
 }
+function blur(){
+    console.log("Hey eyes on me !");
+}
+}
+export default EyesOnMe
